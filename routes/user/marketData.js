87420b881d21a271b009a68a381c0f0ac2845ddb
Nobita_P <tachawin.test@gmail.com>
2019-05-24 19:51:58 +0700

    kuy

--- routes/user/marketData.js
+++ routes/user/marketData.js
@@ -9,7 +9,7 @@ router.get("/",function(req,res){
 
 router.post("/",function(req,res){
     var searchStock = req.body.searchStock;
-    console.log(searchStock);
+    console.log("search  = ",searchStock);
     var findStock = "SELECT * FROM stock_symbol WHERE Stock_Symbol = '"+searchStock+"' ";
     connection.query(findStock,function(err,foundStocks){
         if(err){
@@ -21,13 +21,13 @@ router.post("/",function(req,res){
 })
 
 router.get('/search',function(req,res){
-    connection.query('SELECT Broker_Symbol from trader_account where Broker_Symbol like "%'+req.query.key+'%"', function(err, rows, fields) {
+    connection.query('SELECT Stock_Symbol from stock_symbol where Stock_Symbol like "%'+req.query.key+'%"', function(err, rows, fields) {
           if (err) throw err;
         var data=[];
         for(i=0;i<rows.length;i++)
           {
-            console.log(rows[i].Broker_Symbol);
-            data.push(rows[i].Broker_Symbol);
+            console.log(rows[i].Stock_Symbol);
+            data.push(rows[i].Stock_Symbol);
           }
           res.end(JSON.stringify(data));
         });
